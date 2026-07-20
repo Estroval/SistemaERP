942af58ce3d1eb85a83493f5f8ec0abe03e04f81
Diego <estroval123@gmail.com>
2026-07-20 02:54:28 -0600

    chore: configura Entity Framework Core y conexión a SQL Server

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaERP.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SistemaERPContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
